156c544cd56b9ce801a29492375fbd71ab4aeed8
V Reina <vrodreina@gmail.com>
2017-05-23 20:19:20 -0600

    added email address to html and js for contact

--- formspree.js
+++ formspree.js
@@ -3,7 +3,7 @@ var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '//formspree.io/your@email.com',
+		url: '//formspree.io/vrodreina@gmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
